aa21380a6b4d636393880494ef3c7bb0b0e5c9e1
KKsaten <cocoar147@naver.com>
2024-06-18 15:29:40 +0900

    .

---
+++ src/main/java/com/lsw/app/student/StudentController.java
@@ -0,0 +1,33 @@
+package com.lsw.app.student;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@RequestMapping("/student/list")
+	public String getList(HttpServletRequest request) {
+		request.getParameter("");
+		
+		System.out.println("list 가져오기");
+		studentService.getList();
+
+		
+		return "student/list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/lsw/app/student/StudentDAO.java
@@ -0,0 +1,12 @@
+package com.lsw.app.student;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAO {
+	
+	public void getList() {
+		System.out.println("DAO");
+	}
+
+}
